8857b0f90da642b8fdaf9e0876f1ec8dce735ed7
Dimo Mitev <dimosoftuni@gmail.com>
2025-08-07 21:13:19 +0300

    fix the formatting

--- main.tf
+++ main.tf
@@ -85,9 +85,9 @@ resource "azurerm_mssql_firewall_rule" "firewall" {
 }
 
 resource "azurerm_app_service_source_control" "aassc" {
-  app_id   = azurerm_linux_web_app.alwa.id
-  repo_url = var.repo_url
-  branch   = "main"
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.repo_url
+  branch                 = "main"
   use_manual_integration = true
 }
 
